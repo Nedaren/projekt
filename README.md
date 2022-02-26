fd49ca7c3e89b4624f907f9da5c1a0a7c81de106
Nedaren <kacpurek1999@wp.pl>
2022-02-26 18:14:38 +0100

    .

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Te dane są następnie podawane do funkcji która, za pomocą API udostępnianeg
 
 Aplikacja okienkowa posiada pięć pól w których pojawiają się wartości oraz dwa przyciski. Przycisk "Pobierz Lokację" pobiera wyłącznie dane lokalizacji, podczas gdy "Pobierz Dane" pobiera zarówno dane lokacji oraz dane pogodowe. Pobieranie danych pogodych zajmuje pewien okres czasu, dlatego okienko dialogowe wyskakuje jeżeli przycisk "Pobierz Dane" zostanie naciśnięty przed ukończeniem pobierania danych. 
 
+UWAGA: Program nie jest w stanie funkcjonować w godzinach 0:00 - 2:59. Powodem tego jest iż API 7timer.info zaczyna swój zbiór danych na dany dzień od godziny 3:00.
--- src/main/java/org/kk/projekt/CurrentWeather.java
+++ src/main/java/org/kk/projekt/CurrentWeather.java
@@ -15,17 +15,9 @@ public class CurrentWeather {
 
     public Value getValue (int hour) {
         Value val = new Value();
-        int n;
         for (int i = 0; i < this.apiResponse.dataseries.length; i++) {
             if (this.apiResponse.dataseries[i].timepoint > hour){
-                if (hour < 3) {
-                    n = 3;
-                }
-                // Jeżeli godzina jest mniejsza niż 3, wychodzi błąd ponieważ API nie posiada żadnych danych dla godziny wcześniejszej niż 3:00.
-                else {
-                    n = i - 1;
-                }
-//              API używany do wyznaczenia pogody nie posiada danych na okres
+                int n = i - 1;
                 val.temp = this.apiResponse.dataseries[n].temp2m;
                 val.windDirection = this.apiResponse.dataseries[n].wind10m.direction;
                 val.windSpeed = this.apiResponse.dataseries[n].wind10m.speed;
